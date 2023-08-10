09b5cae2838ab31964c953feec8cdb4461ca5d27
PhucMaii <maithienphuc0102@gmail.com>
2023-08-09 23:56:34 -0700

    install styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import SigninPage from './pages/SigninPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <SigninPage />
