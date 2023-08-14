7888efc7a3716239f62c420ee0520e19c1424f64
PhucMaii <maithienphuc0102@gmail.com>
2023-08-14 16:28:19 -0700

    Refs #15: Signin Page and Sign up Page are linked

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import SigninPage from "./pages/Signin/SigninPage"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import SigninPage from "./pages/Signin/SigninPage";
+import SignupPage from "./pages/Signup/SignupPage";
 
 function App() {
-
   return (
     <>
-      <SigninPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<SignupPage />}/>
+          <Route path='/' element={<SigninPage />}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Signin/styles.jsx
+++ src/pages/Signin/styles.jsx
@@ -1,27 +1,32 @@
-import { FormControl, Grid, Typography } from "@mui/material"
-import styled from "styled-components"
+import { FormControl, Grid, Typography } from "@mui/material";
+import styled from "styled-components";
 
 const GridStyled = styled(Grid)`
-    width: 100%;
-    height: 100%;
-`
+  width: 100%;
+  height: 100%;
+`;
 const TitleStyled = styled(Typography)`
-    color: #854D27;
-    font-weight:bold;
-    margin-bottom: 50px;
-    text-align: center;
-`
+  color: #854d27;
+  font-weight: bold;
+  margin-bottom: 50px;
+  text-align: center;
+`;
 const OutlinedInputStyled = styled(FormControl)`
-    width: 100%;
-`
-
+  width: 100%;
+`;
 const LogoStyled = styled.img`
-    width: 20px;
-    height: 20px;
-    margin-right: 20px;
-`
+  width: 20px;
+  height: 20px;
+  margin-right: 20px;
+`;
 const TopicImageStyled = styled.img`
-    width: '100%'
-`
+  width: "100%";
+`;
 
-export { GridStyled, TitleStyled, OutlinedInputStyled, LogoStyled, TopicImageStyled }
+export {
+  GridStyled,
+  TitleStyled,
+  OutlinedInputStyled,
+  LogoStyled,
+  TopicImageStyled,
+};
