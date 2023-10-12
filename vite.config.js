09618fc9b88a42301fb860f8edeb8c2e64490bef
PhucMaii <maithienphuc0102@gmail.com>
2023-10-12 00:58:01 -0700

    PWA implementation

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,30 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'restaurantapp',
+        short_name: 'restaurantapp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        lang: 'en',
+        scope: '/',
+        icons: [
+          {
+            src: '/image/logo.png',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   define: { 'process.env': process.env },
 });
