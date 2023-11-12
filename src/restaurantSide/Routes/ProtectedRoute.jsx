ed4c18157d8e63a8b53e2b73f445af51fd192260
PhucMaii <maithienphuc0102@gmail.com>
2023-11-11 17:22:47 -0700

    Change Structure of Code

--- src/restaurantSide/Routes/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Outlet, Navigate } from 'react-router-dom';
-import { useState } from 'react';
-import { isAuthenticated } from '../../utils/auth';
-
-export default function ProtectedRoutes() {
-  const [isAuth, _setIsAuth] = useState(isAuthenticated());
-  return isAuth ? <Outlet /> : <Navigate to="/" />;
-}
